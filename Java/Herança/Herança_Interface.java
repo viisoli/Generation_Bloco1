bf4919aeb51eff9c973a43f81d4357896a7e46cb
Vitória Santos Oliveira <contato.santos.vitoria12@gmail.com>
2021-12-16 16:06:55 -0300

    Adicionando Interface Herança

---
+++ Java/Herança/Herança_Interface.java
@@ -0,0 +1,8 @@
+package familia41;
+
+public interface Herança_Interface {
+
+	public String Interface = "Herança_Interface";
+	abstract public String getSom();
+	abstract public String getMovimento();
+}
